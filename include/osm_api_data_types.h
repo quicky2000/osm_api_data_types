442f225a5c9b941baef6791c80b1295df691eaf3
Julien Thevenon <julien_thevenon@yahoo.fr>
2012-10-24 18:36:56 +0200

    Create common include for all osm_objects

---
+++ include/osm_api_data_types.h
@@ -0,0 +1,9 @@
+#ifndef _OSM_API_DATA_TYPES_H_
+#define _OSM_API_DATA_TYPES_H_
+#include "osm_node.h"
+#include "osm_way.h"
+#include "osm_relation.h"
+#include "osm_change.h"
+#include "osm_changeset.h"
+#endif // _OSM_API_DATA_TYPES_H_
+//EOF
